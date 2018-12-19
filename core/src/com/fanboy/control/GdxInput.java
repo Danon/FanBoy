ef389e63dba8f5c38b06dadeff4a520bb6dac830
Daniel Wilkowski <danon@noreply.com>
2018-12-19 19:56:07 +0100

    InputControl accepts arrows as movement

--- core/src/com/fanboy/control/GdxInput.java
+++ core/src/com/fanboy/control/GdxInput.java
@@ -6,19 +6,19 @@ import static com.badlogic.gdx.Input.Keys.*;
 
 public class GdxInput implements InputControl {
     public boolean axisLeft() {
-        return Gdx.input.isKeyPressed(A);
+        return Gdx.input.isKeyPressed(A) || Gdx.input.isKeyPressed(LEFT);
     }
 
     public boolean axisRight() {
-        return Gdx.input.isKeyPressed(D);
+        return Gdx.input.isKeyPressed(D) || Gdx.input.isKeyPressed(RIGHT);
     }
 
     public boolean axisUp() {
-        return Gdx.input.isKeyPressed(W);
+        return Gdx.input.isKeyPressed(W) || Gdx.input.isKeyPressed(UP);
     }
 
     public boolean axisDown() {
-        return Gdx.input.isKeyPressed(S);
+        return Gdx.input.isKeyPressed(S) || Gdx.input.isKeyPressed(DOWN);
     }
 
     public boolean buttonEnter() {
